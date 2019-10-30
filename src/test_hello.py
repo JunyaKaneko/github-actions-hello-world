abe21954226babeb9e4acc0bd90b403b1c1aefb4
Junya Kaneko <JunyaKaneko@users.noreply.github.com>
2019-10-30 14:52:07 +0900

    Update test_hello.py

--- src/test_hello.py
+++ src/test_hello.py
@@ -4,4 +4,4 @@ import hello
 
 class TestHello(unittest.TestCase):
     def test_if_it_returns_correct_sentence(self):
-        self.AssertEqual(hello.hello('Junya'), 'Hello, Junya')
+        self.assertEqual(hello.hello('Junya'), 'Hello, Junya')
